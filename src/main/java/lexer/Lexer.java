b30976468f0f2df13922a616e25594f4a337c05a
lfkdsk <lfk_dsk@hotmail.com>
2017-07-20 00:37:08 +0800

    update lexer

--- src/main/java/lexer/Lexer.java
+++ src/main/java/lexer/Lexer.java
@@ -3,12 +3,26 @@ package lexer;
 import token.Token;
 
 /**
- *
+ * Lexer
+ * File/String Lexer. We wont focus on
+ * the Lexer is a real Lexer or just a
+ * list message.
  * Created by liufengkai on 2017/7/18.
  */
 public interface Lexer {
 
+    /**
+     * peek first serial nodes
+     *
+     * @param index first serial nodes
+     * @return Token
+     */
     Token peek(int index);
 
+    /**
+     * get first node of list
+     *
+     * @return Token
+     */
     Token read();
 }
