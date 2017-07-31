fba11e74b0f00dfa655c0359f601e48d9ebc84c3
lfkdsk <lfk_dsk@hotmail.com>
2017-07-31 10:35:10 +0800

    add comments in number-utils

--- src/main/java/com/lfkdsk/justel/utils/NumberUtils.java
+++ src/main/java/com/lfkdsk/justel/utils/NumberUtils.java
@@ -38,36 +38,19 @@ public class NumberUtils {
         return d;
     }
 
-    public static int toInteger(Object obj) {
-        return (int) obj;
-    }
-
-    public static long toLong(Object obj) {
-        return (long) obj;
-    }
-
-    public static double toDouble(Number num) {
-        if (num instanceof Float) {
-            return Double.parseDouble(num.toString());
-        }
-
-        return num.doubleValue();
-    }
-
-
-    public static int computeIntValue(Object v) {
+    private static int computeIntValue(Object v) {
         return v instanceof Integer ? (Integer) v : 0;
     }
 
-    public static long computeLongValue(Object v) {
+    private static long computeLongValue(Object v) {
         return v instanceof Long ? (long) v : 0;
     }
 
-    public static float computeFloatValue(Object v) {
+    private static float computeFloatValue(Object v) {
         return v instanceof Float ? (Float) v : (int) 0;
     }
 
-    public static double computeDoubleValue(Object v) {
+    private static double computeDoubleValue(Object v) {
         return v instanceof Double ? (double) v : 0;
     }
 
@@ -90,29 +73,28 @@ public class NumberUtils {
                 Math.max(numberValue(l), numberValue(r)));
     }
 
+    /**
+     * Dirty Method to get the real Value of Number
+     *
+     * @param l numObj
+     * @return Real Value
+     */
     public static double computeValue(Object l) {
         return computeIntValue(l) + computeLongValue(l)
                 + computeFloatValue(l) + computeDoubleValue(l);
-    }
-
-    public static Object computePlusToken(NumberToken leftToken, NumberToken rightToken) {
-        return castTokenValue(computeValueToken(leftToken) + computeValueToken(rightToken),
-                Math.max(leftToken.getTag(), rightToken.getTag()));
     }
 
     public static Object computeNegative(Number l) {
         return castTokenValue(-computeValue(l), numberValue(l));
     }
 
-    public static Object computeNegativeToken(NumberToken token) {
-        return castTokenValue(-computeValueToken(token), token.getTag());
-    }
-
-    public static double computeValueToken(NumberToken token) {
-        return (token.integerValue() + token.longValue()
-                + token.floatValue() + token.doubleValue());
-    }
-
+    /**
+     * Cast Token to origin Value
+     *
+     * @param number Num obj
+     * @param flag   Type Tag
+     * @return Number
+     */
     public static Number castTokenValue(Number number, final int flag) {
         switch (flag) {
             case Token.INTEGER:
@@ -127,6 +109,12 @@ public class NumberUtils {
         }
     }
 
+    /**
+     * Cast Token to Value
+     *
+     * @param token Number Token
+     * @return Number
+     */
     public static Number castTokenValue(NumberToken token) {
         switch (token.getTag()) {
             case Token.INTEGER:
@@ -141,7 +129,15 @@ public class NumberUtils {
         }
     }
 
-    public static <T extends Number> int numberValue(T obj) {
+    /**
+     * Get Value in Number-Level Tree
+     * int --> long --> float --> double
+     *
+     * @param obj number obj
+     * @param <T> type extend Number
+     * @return level tag
+     */
+    private static <T extends Number> int numberValue(T obj) {
         if (obj instanceof Integer) {
             return Token.INTEGER;
         } else if (obj instanceof Long) {
@@ -153,6 +149,13 @@ public class NumberUtils {
         }
     }
 
+    /**
+     * num1 & num2
+     *
+     * @param num1 n1
+     * @param num2 n2
+     * @return & value
+     */
     public static Object computeAmpersandValue(Number num1, Number num2) {
         return castTokenValue((long) computeValue(num1) & (long) computeValue(num2),
                 Math.max(numberValue(num1), numberValue(num2)));
